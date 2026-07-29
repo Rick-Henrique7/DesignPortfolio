6c07f840515522e7e06b64333f7e3d1c5de505bf
Rick-Henrique7 <henrique.mdsantos2003@gmail.com>
2026-07-29 20:13:59 -0300

    mudando a cor do sobre mim

--- src/components/ui/radial-background.tsx
+++ src/components/ui/radial-background.tsx
@@ -2,6 +2,6 @@ import { cn } from "../../lib/utils";
 
 export const RadialBackground = () => {
   return (
-    <div className={cn("absolute inset-0 -z-10 size-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)]")} />
+    <div className={cn("absolute inset-0 -z-10 size-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#5a9fd4_100%)]")} />
   );
 };
